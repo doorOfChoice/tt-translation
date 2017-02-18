cbd1f6f8744a43ebc68df1255ee23e0d45bdfa87
doorOfChoice <kevinkiller766@gmail.com>
2017-02-19 01:50:59 +0800

    fix the shell script

--- set.sh
+++ set.sh
@@ -1,10 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 
-cat ~/.bashrc  | grep -q $(pwd) || echo alias tt=\"java -jar "$(pwd)"/trans.jar\" >> ~/.bashrc
+cat ~/.bashrc | grep $(pwd) || echo "alias tt='java -jar $(pwd)/trans.jar'" >> ~/.bashrc
 
-cat ~/.zshrc  | grep -q $(pwd) || echo alias tt=\"java -jar "$(pwd)"/trans.jar\" >> ~/.zshrc
 source ~/.bashrc
-source ~/.zshrc
-#export PATH=$PATH:$(pwd)
-
-#echo alias tt=\"java -jar "$(pwd)"/trans.jar\"
